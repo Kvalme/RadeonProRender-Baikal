84412de5dd18a360fd4e2ae17c36331ca3e28b59
Denis Biryukov <denis.birukov@gmail.com>
2018-06-20 11:16:02 +0300

    Fix Normal/Bump mapping on RPR side

--- Rpr/WrapObject/Materials/UberMaterialObject.cpp
+++ Rpr/WrapObject/Materials/UberMaterialObject.cpp
@@ -88,20 +88,20 @@ void UberMaterialObject::SetInputTexture(const std::string & input_name, Texture
         if (input_name == "uberv2.bump")
         {
             auto sampler = Baikal::InputMap_SamplerBumpMap::Create(input->GetTexture());
-            auto remap = Baikal::InputMap_Remap::Create(
+/*            auto remap = Baikal::InputMap_Remap::Create(
                 Baikal::InputMap_ConstantFloat3::Create(float3(0.0f, 1.0f, 0.0f)),
                 Baikal::InputMap_ConstantFloat3::Create(float3(-1.0f, 1.0f, 0.0f)),
-                sampler);
-            m_mat->SetInputValue("uberv2.shading_normal", remap);
+                sampler);*/
+            m_mat->SetInputValue("uberv2.shading_normal", sampler);
         }
         else if (input_name == "uberv2.normal")
         {
             auto sampler = Baikal::InputMap_Sampler::Create(input->GetTexture());
-            auto remap = Baikal::InputMap_Remap::Create(
+/*            auto remap = Baikal::InputMap_Remap::Create(
                 Baikal::InputMap_ConstantFloat3::Create(float3(0.0f, 1.0f, 0.0f)),
                 Baikal::InputMap_ConstantFloat3::Create(float3(-1.0f, 1.0f, 0.0f)),
-                sampler);
-            m_mat->SetInputValue("uberv2.shading_normal", remap);
+                sampler);*/
+            m_mat->SetInputValue("uberv2.shading_normal", sampler);
         }
         else
         {
